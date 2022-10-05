578059b2884cd5032b42b1cd952361dc66141763
Alessandro <alessandropitocchiap@gmail.com>
2022-10-05 10:03:37 +0200

    Confiurd LSP server

--- lua/opt/keymaps.lua
+++ lua/opt/keymaps.lua
@@ -20,10 +20,11 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-W>h", opts)
+keymap("n", "<C-j>", "<C-W>j", opts)
+keymap("n", "<C-k>", "<C-W>k", opts)
+keymap("n", "<C-l>", "<C-W>l", opts)
+keymap("n", "<C-w>", "<C-W>w", opts)
 
 -- Folder Tree
 keymap("n","<leader>e", ":Lex 30<cr>", opts)
